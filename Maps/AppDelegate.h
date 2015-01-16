4894c2e2cfb0dd5a5cb760558210e2d22e11f3fb
Babatope <babathurpe@yahoo.com>
2015-01-16 14:10:59 -0500

    Initial Commit

---
+++ Maps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maps
+//
+//  Created by Babathurpe on 2015-01-16.
+//  Copyright (c) 2015 Babathurpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Maps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Maps
+//
+//  Created by Babathurpe on 2015-01-16.
+//  Copyright (c) 2015 Babathurpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
